e65fdb86288a224b6737241914bf3eda86d90176
AHOUANDOGBO Amen <ahouandogboamen@gmail.com>
2026-03-03 11:35:55 +0100

    Connexion de plusieurs utilisateurs

--- init-build-test.bat
+++ init-build-test.bat
@@ -1,6 +1,8 @@
 @echo off
 echo   - Running Prisma DB push...
 npx prisma db push --force-reset --accept-data-loss && ^
+echo   - Generating Prisma client... && ^
+npx prisma generate && ^
 echo   - Running seed... && ^
 npx tsx prisma/seed.ts && ^
 echo   - Running test seed... && ^
